d241901b66c5b22ef3660b00ffc3a2799b1e1634
Matt Teichman <teichman@uchicago.edu>
2025-07-02 11:07:02 -0500

    add boilerplate from ocaml.org

--- lib/httpr_cohttp.ml
+++ lib/httpr_cohttp.ml
@@ -1,1 +1,32 @@
-let x = 7
+let ( let* ) = Lwt.bind
+
+let http_get url =
+  let* (resp, body) =
+    Cohttp_lwt_unix.Client.get (Uri.of_string url)
+  in
+  let code = resp
+             |> Cohttp.Response.status
+             |> Cohttp.Code.code_of_status in
+  if Cohttp.Code.is_success code
+  then
+    let* b = Cohttp_lwt.Body.to_string body in
+    Lwt.return (Ok b)
+  else
+    Lwt.return (Error (
+      Cohttp.Code.reason_phrase_of_code code
+    ))
+
+let () =
+  let url = "https://ocaml.org" in
+  Lwt_main.run (
+    let* result = http_get url in
+    match result with
+    | Error str ->
+       Printf.printf "%s:fail\n" url;
+       Printf.printf "Error: %s\n" str;
+       Lwt.return ()
+    | Ok result ->
+       Printf.printf "%s:succed\n" url;
+       print_string result;
+       Lwt.return ()
+  )
