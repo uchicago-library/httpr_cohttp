317857f6aaa685dd2e4d5b59603c3a13832a0b2f
Matt Teichman <teichman@uchicago.edu>
2025-07-03 15:56:35 -0500

    preliminary Cohttp.Response -> Httpr_intf.Response transformation

--- lib/httpr_cohttp.ml
+++ lib/httpr_cohttp.ml
@@ -1,32 +1,79 @@
-let ( let* ) = Lwt.bind
+(* the redirect-chasing code was cribbed from: *)
+(* https://github.com/mirage/ocaml-cohttp/blob/main/README.md#dealing-with-redirects *)
 
-let http_get url =
-  let* (resp, body) =
-    Cohttp_lwt_unix.Client.get (Uri.of_string url)
+let rec http_get_and_follow ~max_redirects uri =
+  let open Lwt.Syntax in
+  let* ans = Cohttp_lwt_unix.Client.get uri in
+  follow_redirect ~max_redirects uri ans
+
+and follow_redirect ~max_redirects request_uri (response, body) =
+  let open Lwt.Syntax in
+  let status = Http.Response.status response in
+  let* () =
+    if status <> `OK then Cohttp_lwt.Body.drain_body body else Lwt.return_unit
   in
-  let code = resp
-             |> Cohttp.Response.status
-             |> Cohttp.Code.code_of_status in
-  if Cohttp.Code.is_success code
-  then
-    let* b = Cohttp_lwt.Body.to_string body in
-    Lwt.return (Ok b)
-  else
-    Lwt.return (Error (
-      Cohttp.Code.reason_phrase_of_code code
-    ))
+  match status with
+  | `OK -> Lwt.return (response, body)
+  | `Permanent_redirect | `Moved_permanently ->
+      handle_redirect ~permanent:true ~max_redirects request_uri response
+  | `Found | `Temporary_redirect ->
+      handle_redirect ~permanent:false ~max_redirects request_uri response
+  | `Not_found | `Gone -> failwith "Not found"
+  | status ->
+      Printf.ksprintf failwith "Unhandled status: %s"
+          (Cohttp.Code.string_of_status status)
 
-let () =
-  let url = "https://ocaml.org" in
-  Lwt_main.run (
-    let* result = http_get url in
-    match result with
-    | Error str ->
-       Printf.printf "%s:fail\n" url;
-       Printf.printf "Error: %s\n" str;
-       Lwt.return ()
-    | Ok result ->
-       Printf.printf "%s:succed\n" url;
-       print_string result;
-       Lwt.return ()
-  )
+and handle_redirect ~permanent ~max_redirects request_uri response =
+  if max_redirects <= 0 then failwith "Too many redirects"
+  else
+    let headers = Http.Response.headers response in
+    let location = Http.Header.get headers "location" in
+    match location with
+    | None -> failwith "Redirection without Location header"
+    | Some url ->
+        let open Lwt.Syntax in
+        let uri = Uri.of_string url in
+        let* () =
+          if permanent then
+            Logs_lwt.warn (fun m ->
+                m "Permanent redirection from %s to %s"
+                  (Uri.to_string request_uri)
+                  url)
+          else Lwt.return_unit
+        in
+        http_get_and_follow uri ~max_redirects:(max_redirects - 1)
+
+let cohttp_to_httpr ~max_redirects uri =
+  let open Cohttp in
+  let open Lwt.Syntax in
+  let* (resp, body_stream) = http_get_and_follow ~max_redirects uri in
+  let headers =
+    let header_alist = resp.headers |> Header.to_list in
+    let each_pair (k, v) = k ^ ": " ^ v in
+    List.map each_pair header_alist
+  in
+  let (status, reason) =
+    let code = Code.code_of_status resp.status in
+    Code.(code,
+          code |> reason_phrase_of_code)
+  in
+  let ctype =
+    match Header.get_media_type resp.headers with
+    | Some ctype -> ctype
+    | None -> "application/octet-stream"
+  in
+  let* body =
+    Cohttp_lwt.Body.to_string body_stream
+  in
+  Lwt.return {
+      Httpr_intf.Response.uri = uri;
+      status;
+      reason;
+      headers;
+      ctype;
+      body;
+    }
+
+let ssl_init _ = ()
+
+let get () = assert false
